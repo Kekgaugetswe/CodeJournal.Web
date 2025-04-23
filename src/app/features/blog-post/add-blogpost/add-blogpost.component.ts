6194056188a642b41a657e8efae19e893ace48dd
Kekgaugetswe <Kekgaugetswe@gmail.com>
2025-04-23 21:13:02 +0200

    submit add BlogPost form

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-blogpost',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-blogpost.component.html',
   styleUrl: './add-blogpost.component.css'
 })
 export class AddBlogpostComponent {
+
+  model: AddBlogPost;
+  constructor() {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      content: '',
+      featuredImageUrl: '',
+      urlHandle: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date()
+    };
+  }
+
+  onFormSubmit() : void{
+
+    console.log('Form submitted', this.model);
+
+  }
 
 }
