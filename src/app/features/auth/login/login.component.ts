275d68bf8fa64a6daaa802ce3e252ca9ef1e42d7
Kekgaugetswe <Kekgaugetswe@gmail.com>
2025-07-20 11:10:56 +0200

    auth service

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -3,20 +3,33 @@ import { LoginRequest } from '../models/login-request.model';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, CommonModule, FormsModule],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   model: LoginRequest;
-  constructor() {
+  constructor(private authService: AuthService) {
     this.model = { email: '', password: '' };
   }
 
   onFormSubmit(): void {
+
+
     console.log(this.model);
+
+    this.authService.login(this.model).subscribe({
+      next: (response) =>{
+        console.log('Login successful', response);
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      }
+    });
   }
+
 }
---
+++ src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { LoginRequest } from '../models/login-request.model';
+import { LoginResponse } from '../models/login-response.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(request: LoginRequest): Observable<LoginResponse>{
+    return  this.http.post<LoginResponse>(`${environment.apiBaseUrl}/api/auth/login`, {
+      email: request.email,
+      password: request.password
+    });
+
+  }
+}
