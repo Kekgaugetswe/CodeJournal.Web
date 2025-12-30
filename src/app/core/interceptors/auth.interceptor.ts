2621db5d9d96c869381eecaf32de6c0da3b953a2
Kekgaugetswe <Kekgaugetswe@gmail.com>
2025-12-30 04:54:54 +0200

    autolog out when the tocken is expired

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,23 +1,35 @@
-import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { inject, Inject } from '@angular/core';
+import { HttpErrorResponse, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../../features/auth/services/auth.service';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
-function shouldInterceptRequest(request: HttpRequest<any>): boolean{
-  return request.urlWithParams.indexOf('addAuth=true', 0) > -1? true: false;
-
+function shouldInterceptRequest(request: HttpRequest<any>): boolean {
+  return request.urlWithParams.indexOf('addAuth=true', 0) > -1;
 }
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const cookieService = inject(CookieService);
-  if(shouldInterceptRequest(req)) {
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
+  let requestToSend = req;
+
+  if (shouldInterceptRequest(req)) {
     const authHeader = cookieService.get('Authorization');
-    const authRequest = authHeader
+    requestToSend = authHeader
       ? req.clone({ setHeaders: { Authorization: authHeader } })
       : req;
-    return next(authRequest);
   }
 
-  return next(req)
+  return next(requestToSend).pipe(
+    catchError((err: unknown) => {
+      if (err instanceof HttpErrorResponse && err.status === 401) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => err);
+    })
+  );
 };
-
